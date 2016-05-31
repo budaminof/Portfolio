b928f4e41d1042f77606a9f0c2b4857724d34166
bud aminof <budaminof@gmail.com>
2016-05-31 17:19:57 -0600

    changed the staructor of the state and nesting to fixed the angular error

--- app/app.module.js
+++ app/app.module.js
@@ -22,22 +22,15 @@
     $urlRouterProvider.otherwise("/");
 
     $stateProvider
-       .state('app', {
-         abstract: true,
-         template: "<my-app></my-app>",
-      })
       .state('portfolio', {
-        parent:'app',
         url:"/",
         templateUrl: "<my-portfolio></my-portfolio>",
      })
       .state('resume', {
-        parent:'app',
         url:"/resume",
         templateUrl: "<my-resume></my-resume>",
      })
       .state('about', {
-        parent:'app',
         url:"/about",
         templateUrl: "<my-about></my-about>",
      })
