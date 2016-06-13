686b9390a07c7a981f7bdf7ca3f6e06303c75ac0
bud aminof <budaminof@gmail.com>
2016-06-12 21:59:14 -0600

    starting to work on text

--- app/projects.service.js
+++ app/projects.service.js
@@ -9,9 +9,9 @@
       function projectsFactory ($log) {
         let _projects = [
           {
-            title: "HangTen",
-            pathState: "hangten",
-            oneSentence: "Surf spot recommendations in real-time",
+            title: "Panic Button",
+            pathState: "panicbutton",
+            oneSentence: "Do you understand what is happening class?",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             images : [
               "http://i.imgur.com/zKewK0r.png",
@@ -23,10 +23,13 @@
             siteUrl: "https://hangten.herokuapp.com/",
             tech: [
               "AngularJS",
+              "Ionic",
+              "Sockets.io",
+              "Google Charts",
               "JSON Web Token",
+              "Hyper Media API",
               "PostgreSQL",
               "Express",
-              "JavaScript"
             ]
           },
           {
